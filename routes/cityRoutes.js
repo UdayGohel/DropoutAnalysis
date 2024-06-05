c22197b065fc90fabdc9614d28fe4fcfd0bc8bd1
Uday Gohel <goheluday6445@gmail.com>
2024-06-06 00:28:34 +0530

    Done city added

--- routes/cityRoutes.js
+++ routes/cityRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const cityController = require("../controllers/city");
+const cityController = require("../Controllers/city");
 
 router.post("/addCities", cityController.addCities);
 router.get("/getCities", cityController.getCities);
